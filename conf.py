50892a39dcc7ce65432b19296540d52ac97f76e1
Cryptoguard <cryptoguard@protonmail.com>
2024-01-26 10:57:08 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -99,7 +99,7 @@ else:
         # on the Pocoo themes), but since we're using the RTD theme
         # for the production docs, it's best to use that to avoid
         # issues due to discrepancies between the themes.
-        html_theme = 'sphinx_rtd_theme'
+        # html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -115,7 +115,7 @@ html_static_path = ['_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
-html_css_files = ['_static/css/custom.css']
+html_css_files = ['css/custom.css']
 
 #def setup(app):
 #    app.add_css_file('_static/css/custom.css')
